0e42afbfeddc2f775d8c0eae62370c65428ba642
MazenSalahb <mazensalah360@gmail.com>
2023-02-16 15:47:58 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+let vidUrl = document.getElementById("url");
+let vidId;
+let imageDiv = document.getElementById("imageDiv")
+let img = document.getElementById("img");
+let a = document.getElementById("downBtn");
+let label = document.getElementById("label");
+let imgsrc;
+
+function getthumb() {
+    if (vidUrl.value.includes("=")) {
+        vidId = vidUrl.value.slice((vidUrl.value.search('v=') + 2),(vidUrl.value.search('v=')+13));
+        imgsrc = `https://img.youtube.com/vi/${vidId}/maxresdefault.jpg`;
+        
+        img.src = imgsrc;
+        imageDiv.style.display = "flex";
+        a.href = imgsrc
+        label.style.display ="none"
+    }else if (vidUrl.value.includes("be")){
+        vidId = vidUrl.value.slice((vidUrl.value.search('be/') + 3));
+        imgsrc = `https://img.youtube.com/vi/${vidId}/maxresdefault.jpg`;
+        img.src = imgsrc;
+        imageDiv.style.display = "flex";
+        a.href = imgsrc
+        label.style.display ="none"
+    }else{
+        vidUrl.style.border ="1px solid red";
+        label.innerHTML = "من فضلك ادخل رابط صحيح"
+        label.style.display ="block"
+    }
+};
+
+
+function UrlExists(url) {
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    if (http.status != 404)
+        return true;
+    else
+        return false;
+}
